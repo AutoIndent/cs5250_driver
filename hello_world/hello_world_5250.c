6e223b918362b02ca1e1b3758a908d8f2fb0a575
Zhang Yuan <zhangyuan.jason@gmail.com>
2019-03-26 01:08:03 +0800

    renamed hello_world c file, added <who> param

--- hello_world/hello_world_5250.c
+++ hello_world/hello_world_5250.c
@@ -2,14 +2,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("GPL");
+
+static char *who = "no_name";
+module_param(who, charp, S_IRUGO);
+
 static int hello_init(void)
 {
-printk(KERN_ALERT "Hello, world\n");
-return 0;
+	printk(KERN_ALERT "Hello, %s\n", who);
+	return 0;
 }
 static void hello_exit(void)
 {
-printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, %s\n", who);
 }
 module_init(hello_init);
 module_exit(hello_exit);
